805f60812c137bc3a13e9af3a166ab5ddef1e88a
bm402 <bncrypted@gmail.com>
2023-01-04 16:37:13 +0000

    00119

---
+++ questions/x0119-medium-replaceall/template.ts
@@ -0,0 +1,5 @@
+type ReplaceAll<S extends string, From extends string, To extends string> = From extends '' 
+    ? S
+    : S extends `${ infer T }${ From }${ infer U }` 
+        ? `${ T }${ To }${ ReplaceAll<U, From, To> }`
+        : S
